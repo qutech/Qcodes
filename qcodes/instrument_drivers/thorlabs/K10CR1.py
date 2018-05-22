f646a5416e0f982b03003ee92e570a239030db02
AG-BLUHM-38\Flash <thomas.descamps@physik.rwth-aachen.de>
2018-05-22 18:24:49 +0200

    now import thorlabs_apt library instead of directly dealing with the .dll file !!!! initialize acceleration = 10. and move_home_velocity=10. !!!! Otherwise the motor rotates too slowly, and error like communication timed out appears the parameter angle seems to work

--- qcodes/instrument_drivers/thorlabs/K10CR1.py
+++ qcodes/instrument_drivers/thorlabs/K10CR1.py
@@ -1,137 +1,114 @@
-from qcodes.instrument.base import Instrument
-from qcodes.instrument.parameter import Parameter
-from qcodes.utils.validators import Numbers
-import ctypes as ct
-import os
-import _error_codes
-import _APTAPI
+# from qcodes.instrument.base import Instrument
+# from qcodes.instrument.parameter import Parameter
+# from qcodes.utils.validators import Numbers
 
-def _get_error_text(error_code):
-    if (error_code == 0):
-        return "Command successful."
-    else:
-        try:
-            return _error_codes.error_message[error_code]
-        except:
-            return "Invalid error code."
+try:
+    import thorlabs_apt as apt
+
+except ImportError as err:
+    raise ImportError('This driver requires thorlabs_apt. You can find it at https://github.com/qpit/thorlabs_apt')
+
+devices = apt.list_available_devices()
+print(devices)
+
+if (50, 55001014) not in devices:
+    raise RuntimeError('Could not find device')
+
+# motor = apt.Motor(55001014)
+#
+# # set default values
+# motor.acceleration = 10.
+# motor.move_home_velocity = 10.
+#
+# motor.move_to(30, True)
+# # motor.move_home(True)
+
+def getHWList():
+    return apt.list_available_devices()
+
+def getHWinfo(serial_number):
+    return apt.hardware_info(serial_number)
 
 class RotaryMount(Instrument):
 
-    def __init__(self, name, serial_number, **kwargs):
+    def __init__(self, name, serial_number, blocking, reference, **kwargs):
 
         super().__init__(name, **kwargs)
-        self._serial_number = serial_number
+        self.serial_number = serial_number
+        self.blocking = blocking
+        self.reference = reference
 
-        #Load the library
-        filename = "%s/APT.dll" % os.path.dirname(__file__)
-        self.lib = ct.windll.LoadLibrary(filename)
-        _APTAPI.set_ctypes_argtypes(self.lib)
-        err_code = self.lib.APTInit()
-        if (err_code == 0):
-            print('Library imported')
-        else:
-            raise Exception("Thorlabs APT Initialization failed: " \
-                            "%s" % _get_error_text(err_code))
-        if (self.lib.EnableEventDlg(False) != 0):
-            raise Exception("Couldn't disable event dialog.")
-
-        #Initialize the device
-        err_code = self.lib.InitHWDevice(self._serial_number)
-        if (err_code != 0):
-            raise Exception("Could not initialize device: %s" %
-                    _get_error_text(err_code))
-        else:
-            print('Device initialization successful !')
+        #Initialize Motor
+        self.mt = apt.Motor(55001014)
+        self.mt.acceleration = 10.
+        self.mt.move_home_velocity = 10.
 
         #Create parameters
         self.add_parameter('angle',
-                           get_cmd = self._current_angle,
-                           set_cmd = self._move_angle,
+                           get_cmd = self._get_angle,
+                           set_cmd = self._set_angle,
                            unit = '°',
                            label = 'Rotary Mount Angle',
-                           vals = Numbers(min_value = 0, max_value = 360),
+                           vals = Numbers(min_value = -360, max_value = 360),
                            docstring = 'Round ND filter')
 
-    def _current_angle(self):
-        pos = ct.c_float()
-        err_code = self.lib.MOT_GetPosition(self._serial_number,
-                ct.byref(pos))
-        if (err_code != 0):
-            raise Exception("Getting position failed: %s" %
-                    _get_error_text(err_code))
-        return pos.value
+    # @property
+    # def serial_number(self):
+    #     return self.serial_number
+    # @serial_number.setter
+    # def serial_number(self, value):
+    #     if not isinstance(value, int):
+    #         raise TypeError('serial_number must be an int')
+    #     self.serial_number = value
 
-    def _move_angle(self, angle, reference = 'absolute', blocking = False):
-        #blocking: bool (wait until homed)
-        if reference not in ['absolute', 'reference']:
+    # @property
+    # def blocking(self):
+    #     return self.blocking
+    # @blocking.setter
+    # def blocking(self, value):
+    #     if not isinstance(value, bool):
+    #         raise TypeError('blocking must be a bool')
+    #     self.blocking = value
+    #
+    # @property
+    # def reference(self):
+    #     return self.reference
+    # @reference.setter
+    # def reference(self, value):
+    #     if not isinstance(value, str):
+    #         raise TypeError('reference must be an str')
+    #     self.reference = value
+
+    def _get_angle(self):
+        return self.mt.position
+
+    def _set_angle(self, value):
+        if self.reference not in ['absolute', 'reference']:
             raise Exception("reference should either be 'absolute' or 'relative'")
-        if reference == 'absolute':
-            err_code = self.lib.MOT_MoveAbsoluteEx(self._serial_number, angle,
-                                               blocking)
-            if (err_code != 0):
-                raise Exception("Setting absolute position failed: %s" %
-                                _get_error_text(err_code))
-        if reference == 'relative':
-            err_code = self.lib.MOT_MoveRelativeEx(self._serial_number, angle,
-                                               blocking)
-            if (err_code != 0):
-                raise Exception("Setting relative position failed: %s" %
-                                _get_error_text(err_code))
-
-    def move_home(self, blocking = False):
-        #blocking: bool (wait until homed)
-        err_code = self.lib.MOT_MoveHome(self._serial_number, blocking)
-        if (err_code != 0):
-            raise Exception("Moving home failed: %s" %
-                    _get_error_text(err_code))
+        if self.reference == 'absolute':
+            self.mt.move_to(value, self.blocking)
+        if self.reference == 'relative':
+            self.mt.move_by(value, self.blocking)
 
     def enable(self):
-        err_code = self.lib.MOT_EnableHWChannel(self._serial_number)
-        if (err_code != 0):
-            raise Exception("Enabling channel failed: %s" %
-                    _get_error_text(err_code))
+        self.mt.enable()
 
     def disable(self):
-        err_code = self.lib.MOT_DisableHWChannel(self._serial_number)
-        if (err_code != 0):
-            raise Exception("Disabling channel failed: %s" %
-                    _get_error_text(err_code))
+        self.mt.disable()
 
-    def list_available_devices(self):
-        devices = []
-        count = ct.c_long()
-        for hwtype in range(100):
-            if (self.lib.GetNumHWUnitsEx(hwtype, ct.byref(count)) == 0):
-                # found an existing hardware type
-                if (count.value > 0):
-                    # get its serial number
-                    serial_number = ct.c_long()
-                    for ii in range(count.value):
-                        if (self.lib.GetHWSerialNumEx(hwtype, ii,
-                                                  ct.byref(serial_number)) == 0):
-                            devices.append((hwtype, serial_number.value))
-        return devices
+    def move_home(self):
+        self.mt.move_home(self.blocking)
 
-    def hardware_info(self):
-        model = ct.c_buffer(255)
-        swver = ct.c_buffer(255)
-        hwnotes = ct.c_buffer(255)
-        err_code = self.lib.GetHWInfo(self._serial_number, model, len(model),
-                                  swver, len(swver), hwnotes, len(hwnotes))
-        if (err_code != 0):
-            raise Exception("Getting hardware info failed: %s" %_get_error_text(err_code))
-        return (model.value, swver.value, hwnotes.value)
-
-mount = RotaryMount('mount', 55001014)
-devices = mount.list_available_devices()
-print(devices)
-info = mount.hardware_info()
-print(info)
-# print(mount.angle())
-# mount.angle()
-# mount._move_angle(50, blocking=True)
-# mount.move_home(False)
-pos = mount._current_angle()
-print(pos)
-# mount.enable()
+# mount = RotaryMount('mount', 55001014, True, 'absolute')
+# devices = mount.list_available_devices()
+# print(devices)
+# info = mount.hardware_info()
+# print(info)
+# # print(mount.angle())
+# # mount.angle()
+# # mount._move_angle(50, blocking=True)
+# # mount.move_home(False)
+# pos = mount._current_angle()
+# print(pos)
+# # mount.enable()
 # mount.move_home()
