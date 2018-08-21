46108f36d4be70a882c4730d31f822c4679bbc6b
AG-BLUHM-38\Flash <thomas.descamps@physik.rwth-aachen.de>
2018-08-21 15:00:28 +0200

    make andor class attributes private. add multitrack and singletrack get and set methods. make acquisition more transparent by separating start_acquisition and wait_for_acqusition. -Tobias

--- qcodes/instrument_drivers/Andor/iDus416.py
+++ qcodes/instrument_drivers/Andor/iDus416.py
@@ -14,6 +14,9 @@ except ImportError:
 # %%
 
 class Andor_iDus(Instrument):
+    """
+    Driver for the Andor iDus 416 CCD
+    """
     def __init__(self, name: str, dll_path: str=None, **kwargs):
         # Initialize the parent Instrument instance
         super().__init__(name, **kwargs)
@@ -24,9 +27,9 @@ class Andor_iDus(Instrument):
         self.add_parameter('acquisition_mode',
                            label='Camera Acquisition Mode',
                            unit=None,
-                           get_cmd=lambda: self.andor.AcquisitionMode,
+                           get_cmd=lambda: self.andor._AcquisitionMode,
                            set_cmd=self.andor.SetAcquisitionMode,
-                           get_parser=lambda x: self._generic_parser('AcquisitionMode'),
+                           get_parser=lambda x: self._generic_parser('_AcquisitionMode'),
                            val_mapping={'Unset': -1,
                                         'Single': 1,
                                         'Accumulate': 2,
@@ -39,9 +42,9 @@ class Andor_iDus(Instrument):
         self.add_parameter('read_mode',
                            label='Camera Read Mode',
                            unit=None,
-                           get_cmd=lambda: self.andor.ReadMode,
+                           get_cmd=lambda: self.andor._ReadMode,
                            set_cmd=self.andor.SetReadMode,
-                           get_parser=lambda x: self._generic_parser('ReadMode'),
+                           get_parser=lambda x: self._generic_parser('_ReadMode'),
                            val_mapping={'Unset': -1,
                                         'FVB': 0,
                                         'Multi-Track': 1,
@@ -56,9 +59,9 @@ class Andor_iDus(Instrument):
         self.add_parameter('trigger_mode',
                            label='Camera Trigger Mode',
                            unit=None,
-                           get_cmd=lambda: self.andor.TriggerMode,
+                           get_cmd=lambda: self.andor._TriggerMode,
                            set_cmd=self.andor.SetTriggerMode,
-                           get_parser=lambda x: self._generic_parser('TriggerMode'),
+                           get_parser=lambda x: self._generic_parser('_TriggerMode'),
                            val_mapping={'Unset': -1,
                                         'Internal': 0,
                                         'External': 1,
@@ -83,35 +86,35 @@ class Andor_iDus(Instrument):
         self.add_parameter('exposure_time',
                            label='Camera Exposure Time',
                            unit='s',
-                           get_cmd=lambda: self.andor.exposure,
+                           get_cmd=lambda: self.andor._exposure,
                            set_cmd=self.andor.SetExposureTime,
                            vals=validators.Numbers())
         
         self.add_parameter('accumulation_cycle_time',
                            label='Camera Accumulation Cycle Time',
                            unit='s',
-                           get_cmd=lambda: self.andor.accumulate,
+                           get_cmd=lambda: self.andor._accumulate,
                            set_cmd=self.andor.SetAccumulationCycleTime,
                            vals=validators.Numbers(min_value=0))
         
         self.add_parameter('kinetic_cycle_time',
                            label='Camera Kinetic Cycle Time',
                            unit='s',
-                           get_cmd=lambda: self.andor.kinetic,
+                           get_cmd=lambda: self.andor._kinetic,
                            set_cmd=self.andor.SetKineticCycleTime,
                            vals=validators.Numbers(min_value=0))
         
         self.add_parameter('number_accumulations',
                            label='Number of Accumulations',
                            unit=None,
-                           get_cmd=lambda: self.andor.accumulations,
+                           get_cmd=lambda: self.andor._accumulations,
                            set_cmd=self.andor.SetNumberAccumulations,
                            vals=validators.Ints(min_value=1))
         
         self.add_parameter('number_kinetics',
                            label='Number of Scans',
                            unit=None,
-                           get_cmd=lambda: self.andor.scans,
+                           get_cmd=lambda: self.andor._scans,
                            set_cmd=self.andor.SetNumberKinetics,
                            vals=validators.Ints(min_value=1))
         
@@ -128,18 +131,43 @@ class Andor_iDus(Instrument):
                            label='Pre-Amplifier Gain',
                            unit=None,
                            get_cmd=self.andor.GetPreAmpGain,
-                           set_cmd=self.andor.SetPreAmpGain,
-                           vals=validators.Ints(min_value=0,
-                                                max_value=self.number_preamp_gains()-1))
+                           set_cmd=self.andor.SetPreAmpGain)
         
         self.add_parameter('n_images',
                            label='Number of New Images',
                            unit=None,
                            get_cmd=self.andor.GetNumberNewImages)
         
+        self.add_parameter('h_shift_speed',
+                           label='Horizontal Shift Speed',
+                           unit=None,
+                           get_cmd=self.andor.GetHSSpeed,
+                           set_cmd=self.andor.SetHSSpeed)
+                           
+        
         self.connect_message()
 
+    def _generic_parser(self, attr: str):
+        """
+        A generic get parser for the various mode settings to accomodate the
+        parameter not being set yet.
+        """
+        val = getattr(self.andor, attr)
+        if val is None:
+            return -1
+        else:
+            return int(val)
+
     def get_idn(self):
+        """
+        Gets the most relevant information about this instrument.
+        
+        Returns a dictionary containing
+            - 'vendor': instrument vendor
+            - 'model': model number
+            - 'serial': serial number
+            - 'firmware': tuple of firmware version and firmware build
+        """
         firmware_version, firmware_build = self.andor.GetHardwareVersion()
         serial = self.andor.GetCameraSerialNumber()
         
@@ -147,13 +175,6 @@ class Andor_iDus(Instrument):
                 'serial': serial,
                 'firmware': (firmware_version, firmware_build)}
         
-    def _generic_parser(self, attr: str):
-        val = getattr(self.andor, attr)
-        if val is None:
-            return -1
-        else:
-            return int(val)
-
     def abort_acquisition(self):
         """
         Description     This function aborts the current acquisition if one is
@@ -198,6 +219,43 @@ class Andor_iDus(Instrument):
                     SetReadMode, SetSingleTrack, SetTriggerMode, SetVSSpeed  
         """
         return self.andor.StartAcquisition()
+
+    def wait_for_acquisition(self):
+        """
+        Description WaitForAcquisition can be called after an acquisition is
+                    started using  StartAcquisition to put the calling thread
+                    to sleep until an Acquisition Event occurs. This can be
+                    used as a simple alternative to the functionality provided
+                    by the SetDriverEvent function, as all Event creation and
+                    handling is performed internally by the SDK library.  
+                    Like the SetDriverEvent functionality it will use less
+                    processor resources than continuously polling with the
+                    GetStatus function. If you wish to restart the calling
+                    thread without waiting for an Acquisition event, call the
+                    function CancelWait. An Acquisition Event occurs each time
+                    a new image is acquired during an Accumulation, Kinetic
+                    Series or Run-Till-Abort acquisition or at the end of a
+                    Single Scan Acquisition. If a second event occurs before
+                    the first one has been acknowledged, the first one will be 
+                    ignored. Care should be taken in this case, as you may have
+                    to use CancelWait to exit the function. 
+        Parameters  NONE 
+        Return      unsigned int    
+                    DRV_SUCCESS         Acquisition Event occurred  
+                    DRV_NOT_INITIALIZED System not initialized. 
+                    DRV_NO_NEW_DATA     Non-Acquisition Event occurred.(e.g.
+                                        CancelWait() called) 
+        
+        See also    StartAcquisition, CancelWait  
+        """
+        self.andor.WaitForAcquisition()
+
+    def acquire(self):
+        """
+        A wrapper to start the acquisition of data and wait for it to finish.
+        """
+        self.start_acquisition()
+        self.wait_for_acquisition()
 
     def get_acquisition_timings(self):
         """
@@ -269,6 +327,17 @@ class Andor_iDus(Instrument):
         """
         return self.andor.GetStatus()
     
+    def get_single_track_settings(self):
+        """
+        Get current single track settings.
+        
+        Return      (centre, height)
+        See also    set_single_track
+        """
+        centre = self.andor._st_centre
+        height = self.andor._st_height
+        return (centre, height)
+    
     def set_single_track(self, centre: int, height: int):
         """
         Description This function will set the single track parameters. The
@@ -289,6 +358,20 @@ class Andor_iDus(Instrument):
         See also    SetReadMode 
         """
         return self.andor.SetSingleTrack(centre, height)
+    
+    def get_multi_track_settings(self):
+        """
+        Get current multi track settings.
+        
+        Return      (centre, height, offset, gap, bottom)
+        See also    set_multi_track
+        """
+        number = self.andor._mt_number
+        height = self.andor._mt_height
+        offset = self.andor._mt_offset
+        gap = self.andor._mt_gap
+        bottom = self.andor._mt_bottom
+        return (number, height, offset, gap, bottom)
     
     def set_multi_track(self, number: int, height: int, offset: int):
         """
@@ -345,7 +428,7 @@ class Andor_iDus(Instrument):
         See also  GetStatus, StartAcquisition, GetAcquiredData16  
         """
         self.andor.GetAcquiredData(data)
-        return self.andor.imageArray
+        return self.andor._imageArray
     
     def get_images(self, data=[]):
         """
@@ -372,12 +455,15 @@ class Andor_iDus(Instrument):
         See also    GetImages16, GetNumberNewImages  
         """
         validfirst, validlast = self.andor.GetImages(data)
-        return self.andor.imageArray, validfirst, validlast
+        return self.andor._imageArray, validfirst, validlast
     
     def take_spectrum(self, data=[]):
         """
-        Starts acquisition of data and retrieves it from the buffer.
+        Description Starts acquisition of data and retrieves it from the buffer.
+        
+        Parameters  data: array to store the acquired data in
+        Returns     data
         """
-        self.start_acquisition()
+        self.acquire()
         return self.get_acquired_data(data)
             
