8a027041a4d70b0e20711e060c425a31ea231524
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2024-06-28 15:17:10 +0200

    Add DelegateInstrumentChannel to namespace

--- src/qcodes/instrument/delegate/__init__.py
+++ src/qcodes/instrument/delegate/__init__.py
@@ -1,5 +1,8 @@
 from qcodes.parameters import GroupedParameter  # noqa: F401
 
 from .delegate_channel_instrument import DelegateChannelInstrument  # noqa: F401
-from .delegate_instrument import DelegateInstrument  # noqa: F401
+from .delegate_instrument import (  # noqa: F401
+    DelegateInstrument,
+    DelegateInstrumentChannel,
+)
 from .instrument_group import InstrumentGroup  # noqa: F401
